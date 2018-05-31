8927a5dccfef5f88224a681456383ffed677dcf4
Sebastian Vetter <seb@roadsi.de>
2018-05-31 00:15:39 -0600

    Add organizers page

--- packages/lektor-jade/lektor_jade.py
+++ packages/lektor-jade/lektor_jade.py
@@ -1,14 +1,58 @@
 import os
 import pathlib
+from itertools import chain
 
 from lektor.pluginsystem import Plugin
 from webassets import Environment, Bundle
+
+from lektor.db import Page
+from lektor import build_programs as bp
 
 
 webasset_env = Environment(
     directory='assets/static',
     url='/static',
 )
+
+
+@bp.buildprogram(Page)
+class PageBuildProgram(bp.PageBuildProgram):
+
+    def iter_child_sources(self):
+        p_config = self.source.datamodel.pagination_config
+        pagination_enabled = p_config.enabled
+        child_sources = []
+
+        # So this requires a bit of explanation:
+        #
+        # the basic logic is that if we have pagination enabled then we
+        # need to consider two cases:
+        #
+        # 1. our build program has page_num = None which means that we
+        #    are not yet pointing to a page.  In that case we want to
+        #    iter over all children which will yield the pages.
+        # 2. we are pointing to a page, then our child sources are the
+        #    items that are shown on that page.
+        #
+        # In addition, attachments and pages excluded from pagination are
+        # linked to the page with page_num = None.
+        #
+        # If pagination is disabled, all children and attachments are linked
+        # to this page.
+        all_children = self.source.children.include_hidden(True).include_undiscoverable(True)
+        if pagination_enabled:
+            if self.source.page_num is None:
+                child_sources.append(self._iter_paginated_children())
+                pq = p_config.get_pagination_query(self.source)
+                child_sources.append(set(all_children) - set(pq))
+                child_sources.append(self.source.attachments)
+            else:
+                child_sources.append(self.source.pagination.items)
+        else:
+            child_sources.append(all_children)
+            child_sources.append(self.source.attachments)
+
+        return chain(*child_sources)
 
 
 def get_bundle_for(ext, filters=None):
